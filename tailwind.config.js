d6f2a496438cceb4a03f232aab73dcb3f89888ef
web-developer-mehede <creativemehede@gmail.com>
2025-08-03 19:49:41 +0600

    header done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['InterVariable', '...defaultTheme.fontFamily.sans'],
+      },
+    },
+  },
+  // ...
+}
